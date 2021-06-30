76fbffbaeb66db0d4f15e7e810b2a17443a74a9b
Nayibedya <nayibedya@gmail.com>
2021-06-30 13:06:46 +0530

    added search

--- src/components/TextArea/TextArea.jsx
+++ src/components/TextArea/TextArea.jsx
@@ -5,8 +5,20 @@ const { Search } = Input;
 function TextArea () {
     const [loading, setLoading] = useState(false);
 
+    function onSearch (e) {
+        setLoading(true)
+        setTimeout(function(){ setLoading(false) }, 3000);
+    }
+
     return (
-        <Search placeholder="Enter Link Here" enterButton="Shorten URL" size="large" loading={loading} />
+        <Search 
+            placeholder="Enter Link Here" 
+            enterButton="Shorten URL" 
+            size="large" 
+            loading={loading}
+            allowClear
+            onSearch={onSearch}
+         />
     );
 }
 
